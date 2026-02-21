ccebbc06ed1cff0b5137e5910294d5af8f450217
ZoDream <648383079@qq.com>
2026-02-21 19:09:24 +0800

    update

--- src/Modules/Blog/Forms/BlogQueryForm.cs
+++ src/Modules/Blog/Forms/BlogQueryForm.cs
@@ -6,12 +6,12 @@ namespace NetDream.Modules.Blog.Forms
     {
         public int Category { get; set; }
         public int User { get; set; }
-        public string Tag { get; set; }
+        public string Tag { get; set; } = string.Empty;
 
         public int Status { get; set; }
         public int Type { get; set; }
-        public string Language { get; set; }
-        public string ProgrammingLanguage { get; set; }
-        public string Pl { get; set; }
+        public string Language { get; set; } = string.Empty;
+        public string ProgrammingLanguage { get; set; } = string.Empty;
+        public string Pl { get; set; } = string.Empty;
     }
 }
